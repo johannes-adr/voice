64c3c205528ea75c403257051bf216a8ee25ca2a
johannes-adr <john.rans@web.de>
2026-04-04 00:21:51 +0200

    gh pages

---
+++ docs/pkg/voice_bg.wasm.d.ts
@@ -0,0 +1,13 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const __wbg_voiceclassifier_free: (a: number, b: number) => void;
+export const voiceclassifier_new: () => [number, number, number];
+export const voiceclassifier_predict: (a: number, b: number, c: number, d: number) => [number, number];
+export const __wbindgen_exn_store: (a: number) => void;
+export const __externref_table_alloc: () => number;
+export const __wbindgen_externrefs: WebAssembly.Table;
+export const __externref_table_dealloc: (a: number) => void;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_start: () => void;
